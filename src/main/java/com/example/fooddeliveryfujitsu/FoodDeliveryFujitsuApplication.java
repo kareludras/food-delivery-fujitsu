399a95f020173517402fd4b085b9c9e63a4b3ff0
karel <udras.karel@gmail.com>
2025-03-22 17:54:16 +0200

    Springboot initializer

---
+++ src/main/java/com/example/fooddeliveryfujitsu/FoodDeliveryFujitsuApplication.java
@@ -0,0 +1,13 @@
+package com.example.fooddeliveryfujitsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryFujitsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryFujitsuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fooddeliveryfujitsu/FoodDeliveryFujitsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fooddeliveryfujitsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryFujitsuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
